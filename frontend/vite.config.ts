ee1e63fe243c7a198a24ba7e4e9a70ee47cfdc2a
sathindud <sathindu.d.zoysa@gmail.com>
2025-09-02 22:18:20 +0530

    SCRUM-87 Initialize front end code

---
+++ frontend/vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+
+// https://vite.dev/config/
+export default defineConfig({
+
+  server: {
+    proxy: {
+      '/api': 'http://localhost:8080',
+      // '/api': 'http://3.133.159.42:80',
+    }
+  },
+
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+  
+})
